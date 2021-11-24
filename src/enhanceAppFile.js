1f041f2973bba435a7b219e624d3c6db9549d95e
Sille Van Landschoot <info@sillevl.be>
2021-11-24 11:49:35 +0100

    Update enhanceAppFile.js

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -2,8 +2,9 @@ import './style.css'
 const Mousetrap = require('mousetrap')
 
 export default ({ Vue }) => {
-    console.log('hello from presentation-mode')
-    Mousetrap.bind('p', () => { 
-        document.querySelector(".theme-container").classList.toggle('presentation-mode')
-    });
+    if(typeof(Mousetrap.bind) === 'function') {
+        Mousetrap.bind('p', () => { 
+            document.querySelector(".theme-container").classList.toggle('presentation-mode')
+        });
+    }
 }
